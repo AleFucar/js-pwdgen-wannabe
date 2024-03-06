f5938e665d8e9e4ab2b6d21eba3fe3776d734c7a
AleFucar <alessandrofucarino02@gmail.com>
2024-03-06 17:43:00 +0100

    ADD: style.css, sistemato script.js, aggiunta stampa a video della password generata

--- script.js
+++ script.js
@@ -9,9 +9,13 @@ console.log("Ok " + username + " " + surname + " stiamo andando bene!")
 
 var color = prompt("Dimmi adesso il tuo colore preferito") //string
 
-var password = prompt("Ok, " + "sto calcolando la password più sicura per te. Scrivi la parola \" VELOCE\" per farmi andare più veloce.") //string
-
-
 var numero = Math.round(Math.random() * 2000); //int
 
-console.log(username + surname + color + numero)  //string
+var password = ("La tua password più (in)sicura per te è " + "\"" + username + surname + color + numero + "\"")
+
+console.log(password)  //string
+
+document.write("<b>" + password + "</b>");
+
+
+
