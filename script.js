249350d2e810aaf16b92b2689050c3122b16247b
AleFucar <alessandrofucarino02@gmail.com>
2024-03-06 16:27:41 +0100

    ADD: index.html, script.js. codice js finito con generatore di numeri da 0 a 2000

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var username = prompt("Ciao! iniziamo con il tuo nome.") //string
+
+console.log("Ciao " + username) //string
+
+var surname = prompt("Adesso dimmi il tuo cognome") //string
+
+console.log("Ok " + username + " " + surname + " stiamo andando bene!")
+
+var color = prompt("Dimmi adesso il tuo colore preferito") //string
+
+var password = prompt("Ok, " + "sto calcolando la password più sicura per te. Scrivi la parola \" VELOCE\" per farmi andare più veloce.") //string
+
+
+var numero = Math.round(Math.random() * 2000); //int
+
+console.log(username + surname + color + numero)  //string
